b15ed67c64403346e217eccfae3961cb70936dd5
amevarashi <ameko.hekisui@gmail.com>
2024-06-02 19:18:54 +0500

    Removed "Got it" button

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -9,13 +9,6 @@ namespace RimWorldModUpdater.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private bool readAbout;
-    public bool ReadAbout
-    {
-        get => readAbout;
-        set => this.RaiseAndSetIfChanged(ref readAbout, value);
-    }
-
     private readonly ObservableAsPropertyHelper<bool> finishedSetup;
     public bool FinishedSetup => finishedSetup.Value;
 
@@ -24,39 +17,23 @@ public class MainWindowViewModel : ViewModelBase
     public ModListViewModel? ModListViewModel { get; private set; }
     public SettingsViewModel SettingsViewModel { get; }
 
-    public ICommand GotItCommand { get; }
     public ICommand ActivateModListView { get; }
 
     public MainWindowViewModel(ResourceDictionary translations)
     {
         Title = $"{translations["Text.WindowTitle"]} - {App.Version}";
 
-        GotItCommand = ReactiveCommand.Create(() => {
-                ReadAbout = true;
-
-                if (FinishedSetup)
-                {
-                    ModListViewModel = new();
-                }
-            });
-
         ActivateModListView = ReactiveCommand.Create(() => ModListViewModel ??= new());
 
         SettingsViewModel = new();
 
         finishedSetup = this
-            .WhenAnyValue(x => x.SettingsViewModel.FinishedSetup, x => x.ReadAbout)
-            .Select(pars => pars.Item1 && pars.Item2)
+            .WhenAnyValue(x => x.SettingsViewModel.FinishedSetup)
             .ToProperty(this, x => x.FinishedSetup);
 
         this.WhenAnyValue(x => x.FinishedSetup)
             .Where(x => x == true)
             .Select(x => new Unit())
             .InvokeCommand(ActivateModListView);
-
-        /*if (ReadAbout && FinishedSetup)
-        {
-            ModListViewModel = new();
-        }*/
     }
 }
